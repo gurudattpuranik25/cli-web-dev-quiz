acb065bc3175e8e457e015bdfaece2ec5d4a2363
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-09-25 19:48:40 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const readlineSync = require("readline-sync");
+var userName;
+function nameFun() {
+  userName = readlineSync.question("Enter your name :");
+}
+
+nameFun();
+
+if (userName === "") {
+  alert("Please enter your name to proceed.");
+  nameFun();
+}
+
+const answers = [
+  "Hyper Text Markup Language",
+  "Cascading Style Sheet",
+  "Inside the head section",
+  "Text-align:center;",
+  "True",
+];
+
+const questionOne = readlineSync.question(
+  "1. What does HTML stand for? \n A. Hyper Tag Markup Language \n B. Hyper Text Markup Language \n C. Hyperlinks Text Mark Language \n D. Hyperlinking Text Marking Language \n"
+);
+
+const questionTwo = readlineSync.question(
+  "2. What does CSS stand for? \n A. Computing Style Sheet \n B. Creative Style System \n C. Cascading Style Sheet \n D. Creative Styling Sheet \n"
+);
+
+const questionThree = readlineSync.question(
+  "3. Where should a CSS file be referenced in a HTML file? \n A. Before any HTML code \n B. After all HTML code \n C. Inside the head section \n D. Inside the body section \n"
+);
+
+const questionFour = readlineSync.question(
+  "4. What is the correct format for aligning written content to the center of the page in CSS? \n A. Text-align:center; \n B. Font-align:center; \n C. Text:align-center; \n D. Font:align-center; \n"
+);
+
+const questionFive = readlineSync.question(
+  "5. Can styles be included in a html file?  \n A. True \n D. False \n"
+);
+
+const userAnswers = [
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
+  questionFive,
+];
+
+let score = 0;
+for (let i = 0; i < userAnswers.length; i++) {
+  if (userAnswers[i].toLowerCase() === answers[i].toLowerCase()) {
+    score++;
+  } else {
+    score;
+  }
+}
+
+console.log(`Hi ${userName}, you scored ${score}/5`);
